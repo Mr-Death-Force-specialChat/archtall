d9814c5ae54a16a7198744451c11395bd4c76f80
MrDeathForce-specialChat <84960714+Mr-Death-Force-specialChat@users.noreply.github.com>
2022-03-27 19:03:02 +0300

    FINALLY IT BOOTS

--- install-chroot.sh
+++ install-chroot.sh
@@ -77,6 +77,6 @@ fi
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
 sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quite"/GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=\/dev\/${ROOT}:${VOLGRP}:allow-discards loglevel=3/g"' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=\/dev\/${ROOT}:${VOLGRP}:allow-discards loglevel=3/g"' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
