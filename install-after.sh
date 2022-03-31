d80dc304a897919c9d4ba8154925c78c3ce4ca9a
MrDeathForce-specialChat <84960714+Mr-Death-Force-specialChat@users.noreply.github.com>
2022-03-31 10:45:39 +0300

    make everything that requires sudo use sudo

--- install-after.sh
+++ install-after.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo enter your password
+sudo id
+
 echo ----------------------------------------------------------------------------------
 echo \|					Installing microcode					  \|
 echo ----------------------------------------------------------------------------------
@@ -7,10 +10,10 @@ echo ---------------------------------------------------------------------------
 proc_type=$(lscpu)
 if grep -E "GenuineIntel" <<< ${proc_type}; then
 	echo "Intel"
-	pacman -S --noconfirm --needed intel-ucode
+	sudo pacman -S --noconfirm --needed intel-ucode
 elif grep -E "AuthenticAMD" <<< ${proc_type}; then
 	echo "Installing AMD microcode"
-	pacman -S --noconfirm --needed amd-ucode
+	sudo pacman -S --noconfirm --needed amd-ucode
 fi
 
 echo ----------------------------------------------------------------------------------
@@ -19,20 +22,20 @@ echo ---------------------------------------------------------------------------
 
 gpu_type=$(lspci)
 if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-	pacman -S --noconfirm --needed nvidia nvidia-xconfig
+	sudo pacman -S --noconfirm --needed nvidia nvidia-xconfig
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-	pacman -S --noconfirm --needed xf86-video-amdgpu
+	sudo pacman -S --noconfirm --needed xf86-video-amdgpu
 elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
-	pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+	sudo pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
-	pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+	sudo pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 fi
 
 echo ----------------------------------------------------------------------------------
 echo \|					Installing KDE						  \|
 echo ----------------------------------------------------------------------------------
 
-pacman -S plasma-meta kde-applications << EEOF
+sudo pacman -S plasma-meta kde-applications << EEOF
 
 
 
@@ -41,6 +44,6 @@ pacman -S plasma-meta kde-applications << EEOF
 
 
 EEOF
-systemctl enable sddm
+sudo systemctl enable sddm
 sed -i 's/\/install-after.sh//g' ~/.bashrc
 echo Done! just reboot
